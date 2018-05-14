2bb2f5fe9de94012c49a0db7677529bacd9058f4
Charlie Lin <hlin266@uwo.ca>
2018-05-14 11:28:44 -0400

    Add project files.

---
+++ hypercompress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hypercompress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
